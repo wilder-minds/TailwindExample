4282ee0696973a013720e5d675af92b825c29859
Shawn Wildermuth <shawn@wildermuth.com>
2021-02-20 20:41:59 -0500

    Initial Version

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Atkinson-Hyperlegible', 'sans-serif'],
+        serif: ['Atkinson-Hyperlegible', 'serif']
+      },
+      screens: {
+        '2xl': '1920px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
